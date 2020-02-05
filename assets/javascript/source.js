a5c241a72f29c74d71741e354d2657ee89f46b75
Nicholas Upshaw <nicholas.upsha@yahoo.com>
2020-02-05 14:58:52 -0500

    First commit Updating portfolio2

---
+++ assets/javascript/source.js
@@ -0,0 +1,10 @@
+// var instance = M.Carousel.getInstance(elem);
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, {
+        numVisible: 3,
+        indicators: true,
+    });
+});
+
